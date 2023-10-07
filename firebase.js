a6adfcfad879ce77d8ee5b729b7290dd7556ec57
Higor <higor.a.santos12@hotmail.com>
2023-10-07 12:19:14 -0300

    File firebase.js created

---
+++ firebase.js
@@ -0,0 +1,6 @@
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { firebase } from "@react-native-firebase/database";
+
+GoogleSignin.configure({
+    webClientId: '962121049730-fs9ia7r3goi2fj8gb8ur4oge37jn8ovj.apps.googleusercontent.com',
+});
